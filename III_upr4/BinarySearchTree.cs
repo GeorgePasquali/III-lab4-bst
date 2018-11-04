9d1568fdcea898e72352d1809c02fdb9dd5716df
gpasquali <george.pasquali@gmail.com>
2018-11-04 19:07:51 +0200

    initial commit

---
+++ III_upr4/BinarySearchTree.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static III_upr4.BinarySearchTree;
+
+namespace III_upr4
+{
+    
+
+    class BinarySearchTree
+    {
+        public Node root { get; set; }
+        private int minWordLength = 2; // IF WE WANT TO REDUCE THE LENGTH OF THE MAX WORD
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public void insertWord(string word)
+        {
+            root = insert(word, root);
+        }
+
+        public void textToTreeView(TreeView treeView)
+        {
+            treeView.Nodes.Clear();
+
+            TreeNode generatedNode = createViewTreeNode(root, "Root");
+            if (generatedNode != null)
+            {
+                treeView.Nodes.Add(generatedNode);
+            }
+            
+        }
+
+        public void printTreeToTextBoxPostorder(TextBox box)
+        {
+            traversePostorder(root, box);
+
+            
+        }
+        public void printTreeToTextBoxInorder(TextBox box)
+        {
+            traverseInorder(root, box);
+
+
+        }
+        public void printTreeToTextBoxPreorder(TextBox box)
+        {
+            traversePreorder(root, box);
+
+
+        }
+
+        private void traversePostorder(Node currentNode, TextBox box)
+        {
+            
+
+            if(currentNode == null)
+            {
+                return;
+            }
+
+            traversePostorder( currentNode.left,  box);
+            
+            traversePostorder( currentNode.right,  box);
+
+            box.AppendText(currentNode.value + ": " + currentNode.count + " \n");
+
+        }
+
+        private void traverseInorder(Node currentNode, TextBox box)
+        {
+
+
+            if (currentNode == null)
+            {
+                return;
+            }
+
+            traverseInorder(currentNode.left, box);
+
+            box.AppendText(currentNode.value + ": " + currentNode.count + " \n");
+
+            traverseInorder(currentNode.right, box);
+
+
+        }
+
+        private void traversePreorder(Node currentNode, TextBox box)
+        {
+
+
+            if (currentNode == null)
+            {
+                return;
+            }
+
+            box.AppendText(currentNode.value + ": " + currentNode.count + " \n");
+
+            traversePreorder(currentNode.left, box);
+
+            traversePreorder(currentNode.right, box);
+
+        }
+
+        private TreeNode createViewTreeNode(Node currentTreeRoot, string positionDesc)
+        {
+            string addedValue = (currentTreeRoot != null) ? currentTreeRoot.value : null;
+            TreeNode currentNode = new TreeNode(positionDesc + ": " +addedValue);
+
+            if (currentTreeRoot.left != null)
+            {
+                currentNode.Nodes.Add(createViewTreeNode(currentTreeRoot.left, "Left"));
+            }
+
+            if (currentTreeRoot.right != null)
+            {
+                currentNode.Nodes.Add(createViewTreeNode(currentTreeRoot.right, "Right"));
+            }
+
+            return currentNode;
+        }
+
+
+        public void addTextToTree(string text)
+        {
+            StringBuilder wordBuffer = new StringBuilder();
+
+
+            int currentChar = 0; // char counter
+            foreach (char singleChar in text)
+            {
+                if (
+                    char.IsWhiteSpace(singleChar) ||
+                    char.IsPunctuation(singleChar) ||
+                    singleChar == '`'
+
+                )
+                {
+                    if (wordBuffer.Length > minWordLength)
+                    {
+
+                        insertWord(wordBuffer.ToString().ToLower());
+                    }
+
+                    wordBuffer.Clear();
+                }
+                else
+                {
+                    wordBuffer.Append(singleChar);
+                }
+
+                currentChar++; // count the characters
+            }
+
+            if ((wordBuffer.Length > 0) && wordBuffer.Length > 2)
+            {
+                insertWord(wordBuffer.ToString().ToLower());
+            }
+        }
+
+        private Node insert(string word,Node currentNode)
+        {
+            if (currentNode == null)
+            {
+                currentNode = new Node(word);
+            }
+            // else if (word < currentNode.value)
+            else if ( word.CompareTo(currentNode.value) < 0) // case must go to left
+            {
+                currentNode.left = insert( word, currentNode.left);
+            }
+            else if ( word.CompareTo(currentNode.value) == 0)
+            {
+                currentNode.count++;
+            }
+            else
+            {
+                currentNode.right = insert(word, currentNode.right);
+            }
+
+            return currentNode;
+        }
+
+        public void traverse(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            traverse(root.left);
+            traverse(root.right);
+        }
+
+        internal class Node
+        {
+            public string value;
+            public Node left;
+            public Node right;
+            public int count;
+
+            public Node(string inputValue)
+            {
+                value = inputValue;
+                count = 1;
+            }
+
+            public void incrementCount()
+            {
+                count++;
+            }
+        }
+    }
+
+
+    
+}
---
+++ III_upr4/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace III_upr4
+{
+
+    public partial class Form1 : Form
+    {
+
+        private BinarySearchTree myBinaryTree;
+        private string currentString;
+        private Benchmarker myTester;
+
+        public Form1()
+        {
+            InitializeComponent();
+            myTester = new Benchmarker(textBox5);
+        }
+
+        private void  pushTextInsideTreeView()
+        {
+            myBinaryTree = new BinarySearchTree();
+
+            myTester.StartBenchmark();
+            myBinaryTree.addTextToTree(currentString);
+            myTester.EndBenchmark();
+
+            myBinaryTree.textToTreeView(treeView1);
+            
+
+        }
+
+        private void clearBoxes()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            myBinaryTree = null;
+        }
+
+        private void pushTextInMainTextView()
+        {
+            myTester.StartBenchmark();
+            textBox1.Text = currentString;
+            myTester.EndBenchmark();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentString = "The Online Etymology Dictionary gives the first attested use of \"computer\" in the \"1640s, [meaning] \"one who calculates,\"; this is an \"... agent noun from compute (v.)\". The Online Etymology Dictionary states that the use of the term to mean \"calculating machine\" (of any type) is from 1897.\" The Online Etymology Dictionary indicates that the \"modern use\" of the term, to mean \"programmable digital electronic computer\" dates from \"... 1945 under this name; [in a] theoretical[sense] from 1937, as Turing machine";
+            clearBoxes();
+            pushTextInMainTextView();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentString = "Computer programming is the process of writing or editing source code. Editing source code involves testing, analyzing, refining, and sometimes coordinating with other programmers on a jointly developed program. A person who practices this skill is referred to as a computer programmer, software developer, and sometimes coder. The sometimes lengthy process of computer programming is usually referred to as software development.The term software engineering is becoming popular as the process is seen as an engineering discipline. ";
+            clearBoxes();
+            pushTextInMainTextView();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentString = "The word software was first used in the late 1960s to show the difference from computer hardware, which are the parts of a machine that can be seen and touched. Software is the instructions that the computer follows. Before compact discs (CDs) or Internet downloads, software came on various computer data storage media like paper punch cards, magnetic discs or magnetic tape";
+            clearBoxes();
+            pushTextInMainTextView();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            currentString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+            clearBoxes();
+            pushTextInMainTextView();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            currentString = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+            clearBoxes();
+            pushTextInMainTextView();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pushTextInsideTreeView();
+            treeView1.ExpandAll();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            myTester.StartBenchmark();
+            if(myBinaryTree != null)
+            {
+                myBinaryTree.printTreeToTextBoxPostorder(textBox2);
+            }
+            else
+            {
+                showEmptyTreeError();
+            }
+            
+            myTester.EndBenchmark();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            myTester.StartBenchmark();
+            if (myBinaryTree != null)
+            {
+                myBinaryTree.printTreeToTextBoxInorder(textBox3);
+            }
+            else
+            {
+                showEmptyTreeError();
+            }
+            myTester.EndBenchmark();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+            myTester.StartBenchmark();
+            if (myBinaryTree != null)
+            {
+                myBinaryTree.printTreeToTextBoxPreorder(textBox4);
+            }
+            else
+            {
+                showEmptyTreeError();
+            }
+            myTester.EndBenchmark();
+        }
+
+
+        private void showEmptyTreeError()
+        {
+
+            string message = "The tree is still empty, You should parse The text first.";
+            string title = "You missed a step!";
+            MessageBox.Show(message, title);
+
+        }
+    }
+}
